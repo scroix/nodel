326fc650a0d0a7878edd9d9398f051e21c3aaad7
scroix <julien.dsc@gmail.com>
2024-04-07 16:30:51 +1000

    Add more tests
    - Check for source files
    - Check for Google font, which gets packaged indepdently
    - Check for results of custom.css getting correctly compiled
    - Check for bootstrap layout

--- nodel-jyhost/src/test/java/org/nodel/PlaywrightTests.java
+++ nodel-jyhost/src/test/java/org/nodel/PlaywrightTests.java
@@ -37,6 +37,54 @@ public class PlaywrightTests {
         assertEquals("Locals", activeNavItemText, "Active navigation item should be 'Locals'");
     }
 
+    @Test
+    public void testSourceFileAvailability() {
+        APIResponse response = page.request().get("http://127.0.0.1:8085/v1/css/components.default.css");
+        assertEquals(200, response.status(), "components.default.css should be available");
+
+        response = page.request().get("http://127.0.0.1:8085/v1/img/logo.png");
+        assertEquals(200, response.status(), "logo.png should be available");
+
+        response = page.request().get("http://127.0.0.1:8085/v1/js/components.min.js");
+        assertEquals(200, response.status(), "components.min.js should be available");
+
+        response = page.request().get("http://127.0.0.1:8085/v1/js/nodel.js");
+        assertEquals(200, response.status(), "nodel.js should be available");
+    }
+
+    @Test
+    public void testFontFamily() {
+        String fontFamily = page.evaluate("() => window.getComputedStyle(document.body).getPropertyValue('font-family')").toString();
+        assertTrue(fontFamily.contains("Roboto"), "Font family should include Roboto");
+    }
+
+    @Test
+    public void testBootstrapLayout() {
+        assertNotNull(page.querySelector("div.row > div.col-sm-12"), "Bootstrap row and column should be present");
+    }
+
+    @Test
+    public void testNodelAddElement() {
+        assertNotNull(page.querySelector(".nodel-add"), ".nodel-add should be present");
+    }
+
+    @Test
+    public void testListGroupItems() {
+        assertNotNull(page.querySelector(".list-group-item"), "list-group-item elements should be present");
+    }
+
+    @Test
+    public void testListGroupItemBorder() {
+        String border = page.evaluate("() => window.getComputedStyle(document.querySelector('.list-group-basic .list-group-item')).getPropertyValue('border')").toString();
+        assertEquals("none", border, "list-group-item elements should have border: none");
+    }
+
+    @Test
+    public void testNodelAddButtonMargin() {
+        String marginBottom = page.evaluate("() => window.getComputedStyle(document.querySelector('.nodel-add .btn')).getPropertyValue('margin-bottom')").toString();
+        assertEquals("5px", marginBottom, ".nodel-add .btn should have margin-bottom: 5px");
+    }
+
     @AfterAll
     public static void tearDown() {
         if (browser != null) {
